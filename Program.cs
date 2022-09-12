b5c413be132bffcab441486a5a90fe6860433f75
Korisnik <Korisnik@DESKTOP-Q60U0Q5>
2022-09-12 11:16:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+  //  Napišite program u kojem ćete deklarirati varijable a i b tipa int, te im dodijelite vrijednosti 100 (a) i 3 (b). Ispišite njihove vrijednosti u sljedećem obliku://
+//“a dijeljeno s b je x i ostatak y”.//
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            Console.WriteLine(a + " dijeljeno s " + b + " je " + (a / b) + " i ostatak " + (a % b)+" .");
+       
+            Console.ReadLine();
+        }
+    }
+}
